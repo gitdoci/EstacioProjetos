5a5835ecada0b2118c661ef1a1b4603179b2e1c9
Cícero Näscimento <ciceromeucontato@gmail.com>
2025-04-17 03:20:23 +0000

    criei o programa

---
+++ DesafioNovatoSuperTrunfo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    int main(){
+        char estado;
+        char codigo;
+        char cidade;
+        int populacao;
+        float area;
+        float pib;
+        int pontoturisticos;
+
+        
+
+
+
+    }
