37bc44ed664c34ab1b6e59ede62eb9484bf18a27
Cícero Näscimento <ciceromeucontato@gmail.com>
2025-04-17 03:31:30 +0000

    iniciei o projeto

---
+++ super_trunfo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    int main(){
+
+        char estado1, codigo1[4], cidade1[50];
+        int populacao1, pontoturisticos1;
+        float area1, pib1;
+        
+        
+
+
+    }
